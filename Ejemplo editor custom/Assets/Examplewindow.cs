f8b37be9e71b8b45f43c447a912104271fbb3558
jroca7 <70997708+jroca7@users.noreply.github.com>
2021-04-14 22:27:03 -0300

    creacion del archivo

---
+++ Ejemplo editor custom/Assets/Examplewindow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Examplewindow : EditorWindow
+{
+
+    Color Color;
+
+    [MenuItem("Window/Colorear")]
+    public static void ShowWindow()
+    {
+
+        GetWindow<Examplewindow>("Colorear");
+    }
+    void OnGUI()
+    {
+        GUILayout.Label("Colorea la esfera", EditorStyles.boldLabel);
+
+       Color = EditorGUILayout.ColorField("Color", Color);
+
+        if (GUILayout.Button("Colorear"))
+        {
+         
+            Colorize();
+        }
+    }
+    void Colorize()
+    {
+        foreach (GameObject obj in Selection.gameObjects)
+
+        {
+            Renderer renderer = obj.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.sharedMaterial.color = Color;
+            }
+        }
+
+    }
+}
